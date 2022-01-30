5679e00ced636698c89167d3740fe48eaf15db57
Alessandro Credidio Furlan <64380379+alessandrofurlan@users.noreply.github.com>
2022-01-30 13:59:15 -0300

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,3 +1,5 @@
+import pygame
+
 # GLOBALS VARS
 s_width = 800
 s_height = 700
@@ -7,3 +9,123 @@ block_size = 30
 
 top_left_x = (s_width - play_width) // 2
 top_left_y = s_height - play_height
+
+S = [['.....',
+      '.....',
+      '..00.',
+      '.00..',
+      '.....'],
+     ['.....',
+      '..0..',
+      '..00.',
+      '...0.',
+      '.....']]
+
+Z = [['.....',
+      '.....',
+      '.00..',
+      '..00.',
+      '.....'],
+     ['.....',
+      '..0..',
+      '.00..',
+      '.0...',
+      '.....']]
+
+I = [['..0..',
+      '..0..',
+      '..0..',
+      '..0..',
+      '.....'],
+     ['.....',
+      '0000.',
+      '.....',
+      '.....',
+      '.....']]
+
+O = [['.....',
+      '.....',
+      '.00..',
+      '.00..',
+      '.....']]
+
+J = [['.....',
+      '.0...',
+      '.000.',
+      '.....',
+      '.....'],
+     ['.....',
+      '..00.',
+      '..0..',
+      '..0..',
+      '.....'],
+     ['.....',
+      '.....',
+      '.000.',
+      '...0.',
+      '.....'],
+     ['.....',
+      '..0..',
+      '..0..',
+      '.00..',
+      '.....']]
+
+L = [['.....',
+      '...0.',
+      '.000.',
+      '.....',
+      '.....'],
+     ['.....',
+      '..0..',
+      '..0..',
+      '..00.',
+      '.....'],
+     ['.....',
+      '.....',
+      '.000.',
+      '.0...',
+      '.....'],
+     ['.....',
+      '.00..',
+      '..0..',
+      '..0..',
+      '.....']]
+
+T = [['.....',
+      '..0..',
+      '.000.',
+      '.....',
+      '.....'],
+     ['.....',
+      '..0..',
+      '..00.',
+      '..0..',
+      '.....'],
+     ['.....',
+      '.....',
+      '.000.',
+      '..0..',
+      '.....'],
+     ['.....',
+      '..0..',
+      '.00..',
+      '..0..',
+      '.....']]
+
+Font=pygame.font.SysFont('timesnewroman',  30) # setting the pygame font style(1st parameter) and size of font(2nd parameter)
+ 
+# RGB values for white, green, yellow, orange colour
+white=(255, 255, 255)
+yellow=(255, 255, 0)
+green=(0, 255, 255)
+orange=(255, 100, 0)
+
+qops = []
+qops.append(Font.render("X", True, orange, yellow))
+qops.append(Font.render("Y", False, orange, green))
+qops.append(Font.render("Z", False, orange, yellow))
+qops.append(Font.render("Rx", False, orange, green))
+qops.append(Font.render("Ry", False, orange, yellow))
+qops.append(Font.render("Rz", False, orange, green))
+qops.append(Font.render("H", False, orange, yellow))
+qops.append(Font.render("I", False, orange, yellow))
